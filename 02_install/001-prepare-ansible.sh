50a426c512cd287147281311e8cf211e4a0d560c
Tobias Klauenberg <toklaui@live.de>
2022-09-18 10:54:59 +0200

    update ansible and easier prepare execution

--- 02_install/001-prepare-ansible.sh
+++ 02_install/001-prepare-ansible.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 AVAILABLE_HOSTS=$(grep "\." pi-hosts.txt | awk '{print $2}' | cut -d "=" -f2)
+# test connection so that it can be accepted and then do all installments parallel
+for HOST in $AVAILABLE_HOSTS; do
+  echo "check initial connection"
+  ssh -t root@$HOST 'echo "working"'
+done
 for HOST in $AVAILABLE_HOSTS; do
   echo "Working on host $HOST"
-  ssh -t root@$HOST 'setup-ntp -c chrony; setup-apkrepos -f; apk update; apk add python3'
+  ssh -t root@$HOST 'setup-ntp -c chrony; setup-apkrepos -f; apk update; apk add python3' &
 done
+exit
