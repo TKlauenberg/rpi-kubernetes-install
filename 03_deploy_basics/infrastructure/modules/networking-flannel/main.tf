f15567bdac69edc6e79ff8c0c58881422c01d45e
Tobias Klauenberg <toklaui@live.de>
2022-12-16 13:53:10 +0100

    add monitoring and fix networking issues

--- 03_deploy_basics/infrastructure/modules/networking-flannel/main.tf
+++ 03_deploy_basics/infrastructure/modules/networking-flannel/main.tf
@@ -1,15 +1,36 @@
 # kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 
-data "kubectl_file_documents" "flannel_base_files" {
-  content = file("${path.module}/../../external/flannel/Documentation/kube-flannel.yml")
+data "kubectl_file_documents" "flannel_base_files" { content = file("${path.module}/../../external/flannel/Documentation/kube-flannel.yml") }
+
+locals {
+  filtercfg = {
+    for key, val in data.kubectl_file_documents.flannel_base_files.manifests :
+    key => val
+    if yamldecode(val).metadata.name != "kube-flannel-cfg"
+  }
+  filterds = {
+    for key, val in local.filtercfg :
+    key => val
+    if yamldecode(val).metadata.name != "kube-flannel-ds"
+  }
+  dsvalue = [
+    for key, val in data.kubectl_file_documents.flannel_base_files.manifests :
+    val
+    if yamldecode(val).metadata.name == "kube-flannel-ds"
+  ]
+  flannel_base_config = [
+    for key, val in data.kubectl_file_documents.flannel_base_files.manifests :
+    val
+    if yamldecode(val).metadata.name == "kube-flannel-cfg"
+  ]
 }
 
 resource "kubectl_manifest" "flannel_networking" {
-  for_each  = data.kubectl_file_documents.flannel_base_files.manifests
+  for_each  = local.filterds
   yaml_body = each.value
 }
 
-resource "kubernetes_config_map_v1_data" "flannel_networking_patch" {
+resource "kubernetes_config_map_v1" "flannel_networking" {
   depends_on = [
     kubectl_manifest.flannel_networking
   ]
@@ -17,8 +38,15 @@ resource "kubernetes_config_map_v1_data" "flannel_networking_patch" {
     name      = "kube-flannel-cfg"
     namespace = "kube-flannel"
   }
-  force = true
   data = {
+    "cni-conf.json" = yamldecode(local.flannel_base_config[0]).data["cni-conf.json"]
     "net-conf.json" = "{\r\n\t\"Network\": \"10.244.0.0/16\",\r\n\t\"Backend\": { \r\n\t\t\"Type\": \"host-gw\"\n\t}\n}"
   }
+}
+
+resource "kubectl_manifest" "flannel_networking_ds" {
+  depends_on = [
+    kubernetes_config_map_v1.flannel_networking
+  ]
+  yaml_body = local.dsvalue[0]
 }
---
+++ 03_deploy_basics/infrastructure/monitoring.tf
@@ -0,0 +1,359 @@
+resource "kubernetes_namespace" "monitoring-namespace" {
+  metadata {
+    name = "monitoring"
+    labels = {
+      app = "monitoring"
+    }
+  }
+}
+
+locals {
+  influx_name          = "influx-monitor"
+  namespace_name       = kubernetes_namespace.monitoring-namespace.metadata[0].name
+  influx_template_name = "influx-template"
+  influx_bucket        = "metrics"
+  influx_organization  = "monitor"
+}
+
+resource "kubernetes_secret" "influxdb_template" {
+  depends_on = [
+    kubernetes_namespace.monitoring-namespace
+  ]
+  metadata {
+    name      = local.influx_template_name
+    namespace = local.namespace_name
+  }
+  data = {
+    "k8s.yml" = file("${path.root}/static/external/community-templates/k8s/k8s.yml")
+  }
+}
+
+resource "helm_release" "influxdb2" {
+  depends_on = [
+    module.networking_flannel,
+    module.networking_metallb,
+    helm_release.ingress-nginx,
+    kubernetes_namespace.monitoring-namespace,
+    helm_release.nfs_provisioner,
+  ]
+  name       = "influxdb"
+  repository = "https://helm.influxdata.com"
+  chart      = "influxdb2"
+  namespace  = local.namespace_name
+
+  set {
+    name  = "image.tag"
+    value = "2.5.1-alpine"
+  }
+
+  values = [yamlencode({
+    nameOverride = local.influx_name
+    resources = {
+      requests = {
+        cpu    = "200m"
+        memory = "256Mi"
+      }
+      limits = {
+        cpu    = "1"
+        memory = "2Gi"
+      }
+    }
+    adminUser = {
+      organization = local.influx_organization
+      bucket       = local.influx_bucket
+      user         = "admin"
+    }
+    volumes = [{
+      name = "influx-template"
+      secret = {
+        secretName = kubernetes_secret.influxdb_template.metadata[0].name
+      }
+    }]
+    mountPoints = [{
+      name      = "influx-template"
+      mountPath = "/influxdb2-templates"
+      readOnly  = true
+    }]
+    initScripts = {
+      enabled = true
+      scripts = {
+        "init.sh" = <<-EOT
+        #!/bin/bash
+        influx apply --force yes -f /influxdb2-templates/k8s.yml
+        EOT
+      }
+    }
+    ingress = {
+      enabled   = true
+      className = "nginx"
+      hostname  = "influxdb.cluster.local"
+      path      = "/"
+    }
+  })]
+}
+
+data "kubernetes_service" "influxdb" {
+  depends_on = [
+    helm_release.influxdb2
+  ]
+  metadata {
+    name      = "influxdb-${local.influx_name}"
+    namespace = local.namespace_name
+  }
+}
+
+data "kubernetes_secret" "influxdb_secrets" {
+  depends_on = [
+    helm_release.influxdb2
+  ]
+  metadata {
+    name      = "influxdb-${local.influx_name}-auth"
+    namespace = local.namespace_name
+  }
+
+}
+
+
+locals {
+  influxdb_svc_name = data.kubernetes_service.influxdb.metadata[0].name
+  influxdb_port     = data.kubernetes_service.influxdb.spec[0].port[index(data.kubernetes_service.influxdb.spec[0].port.*.name, "http")].port
+  influxdb_token    = data.kubernetes_secret.influxdb_secrets.data.admin-token
+}
+
+resource "helm_release" "kube-state-metrics" {
+  depends_on = [
+    helm_release.influxdb2
+  ]
+  name       = "kube-state-metrics"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-state-metrics"
+  namespace  = "kube-system"
+
+
+  values = [yamlencode({
+    config = {
+      resources = {
+        requests = {
+          cpu    = "100m"
+          memory = "128Mi"
+        }
+        limits = {
+          cpu    = "200m"
+          memory = "256Mi"
+        }
+      }
+    }
+  })]
+}
+
+
+resource "helm_release" "telegraf-ds" {
+  depends_on = [
+    helm_release.influxdb2
+  ]
+  name       = "telegraf-ds"
+  repository = "https://helm.influxdata.com"
+  chart      = "telegraf-ds"
+  namespace  = local.namespace_name
+
+
+  values = [
+    yamlencode({
+      config = {
+        resources = {
+          requests = {
+            cpu    = "100m"
+            memory = "128Mi"
+          }
+          limits = {
+            cpu    = "200m"
+            memory = "256Mi"
+          }
+        }
+      }
+    }),
+    yamlencode({
+      config = {
+        outputs = [
+          {
+            influxdb_v2 = {
+              urls         = ["http://${local.influxdb_svc_name}:${local.influxdb_port}"]
+              token        = local.influxdb_token
+              bucket       = local.influx_bucket
+              organization = local.influx_organization
+            }
+          }
+        ]
+      }
+    }),
+  ]
+
+  # alpine image is not compatible with arm64/v8
+  set {
+    name  = "image.tag"
+    value = "1.24"
+  }
+
+  # default socket is doocker socket in telegraf.
+  # so we set it to crio socket
+  set {
+    name  = "config.docker_endpoint"
+    value = ""
+  }
+}
+
+locals {
+  telegraf_svc_account = "telegraf-svc-account"
+}
+
+resource "kubernetes_cluster_role" "influx_cluster_viewer" {
+  depends_on = [
+    kubernetes_namespace.monitoring-namespace,
+    helm_release.telegraf-ds,
+  ]
+  metadata {
+    name = "influx:cluster:viewer"
+    labels = {
+      "rbac.authorization.k8s.io/aggregate-view-telegraf" = true
+    }
+  }
+  rule {
+    api_groups = [""]
+    resources  = ["persistentvolumes", "nodes"]
+    verbs      = ["get", "list"]
+  }
+}
+
+resource "kubernetes_cluster_role" "influx_telegraf" {
+  depends_on = [
+    kubernetes_cluster_role.influx_cluster_viewer,
+  ]
+  metadata {
+    name = "influx:telegraf:aggregate"
+  }
+  aggregation_rule {
+    cluster_role_selectors {
+      match_labels = {
+        "rbac.authorization.k8s.io/aggregate-view-telegraf" = true
+
+      }
+    }
+    cluster_role_selectors {
+      match_labels = {
+        "rbac.authorization.k8s.io/aggregate-to-view" = true
+      }
+    }
+  }
+}
+
+resource "kubernetes_secret_v1" "influx_telegraf_svc_account" {
+  metadata {
+    name = "${local.telegraf_svc_account}-secret"
+    namespace = local.namespace_name
+    annotations = {
+      "kubernetes.io/service-account.name" = local.telegraf_svc_account
+    }
+  }
+  type = "kubernetes.io/service-account-token"
+}
+
+resource "kubernetes_service_account" "influx_telegraf" {
+  depends_on = [
+    kubernetes_namespace.monitoring-namespace,
+    helm_release.telegraf-ds,
+  ]
+  metadata {
+    name      = local.telegraf_svc_account
+    namespace = local.namespace_name
+  }
+  secret {
+    name = kubernetes_secret_v1.influx_telegraf_svc_account.metadata.0.name
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "influx_telegraf_binding" {
+  depends_on = [
+    kubernetes_service_account.influx_telegraf,
+    kubernetes_cluster_role.influx_telegraf
+  ]
+  metadata {
+    name = "influx:telegraf:${local.telegraf_svc_account}"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "influx:telegraf:aggregate"
+  }
+  subject {
+    kind      = "ServiceAccount"
+    name      = local.telegraf_svc_account
+    namespace = local.namespace_name
+  }
+}
+
+resource "helm_release" "telegraf" {
+  depends_on = [
+    helm_release.influxdb2,
+    kubernetes_cluster_role_binding.influx_telegraf_binding,
+  ]
+  name       = "telegraf"
+  repository = "https://helm.influxdata.com"
+  chart      = "telegraf"
+  namespace  = local.namespace_name
+
+  values = [
+    yamlencode({
+      resources = {
+        requests = {
+          cpu    = "100m"
+          memory = "128Mi"
+        }
+        limits = {
+          cpu    = "200m"
+          memory = "256Mi"
+        }
+      }
+    }),
+    yamlencode({
+      config = {
+        outputs = [
+          {
+            influxdb_v2 = {
+              urls         = ["http://${local.influxdb_svc_name}:${local.influxdb_port}"]
+              token        = local.influxdb_token
+              bucket       = local.influx_bucket
+              organization = local.influx_organization
+            }
+          }
+        ]
+        inputs = [
+          {
+            kube_inventory = {
+              url                  = "https://kubernetes.default.svc"
+              namespace            = ""
+              bearer_token         = "/var/run/secrets/kubernetes.io/serviceaccount/token"
+              insecure_skip_verify = true
+            }
+          }
+        ]
+      }
+      rbac = {
+        create = false
+      }
+      serviceAccount = {
+        create = false
+        name   = local.telegraf_svc_account
+      }
+      # disable service because it isn's used in this configuration
+      service = {
+        enabled = false
+      }
+    }),
+  ]
+
+  # alpine image is not compatible with arm64/v8
+  set {
+    name  = "image.tag"
+    value = "1.24"
+  }
+}
